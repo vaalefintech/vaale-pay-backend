98cdf4629700d76022a21f218569aeefaaa7915d
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2023-07-09 17:22:15 -0500

    backend ok

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,9 +13,9 @@ variable "region" {
   default     = "us-east-1"
 }
 
-# terraform apply -var zipfile="../zips/build_20230709_164904.zip"
+# terraform apply -var zipfile="../zips/build_20230709_171845.zip"
 variable "zipfile" {
   description = "Zip File"
   type        = string
-  default     = "../zips/build_20230709_164904.zip"
+  default     = "../zips/build_20230709_171845.zip"
 }
